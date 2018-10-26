09f73f0bf26ee5d97a4470577fcc42e14dcd6e46
Hana87NH <hanhhoang87@gmail.com>
2018-10-25 23:49:47 -0400

    Modified code.R and added 2 Pareto graphs

--- code/code.R
+++ code/code.R
@@ -21,6 +21,14 @@ hist(citibike$birth.year, main='Histogram of Citi Bike users birth year', xlab='
 boxplot(citibike$starttime, main='Boxplot of pick up time in POSIXct time')
 boxplot(citibike$stoptime, main='Boxplot of drop off time in POSIXct time')
 hist(citibike$gender, main='Histogram of users gender', xlab='Gender (0=unknown; 1=male; 2=female)')
+boxplot(citibike$birth.year, main='Boxplot of users birth year')
+
+install.packages('qcc')
+library(qcc)
+pareto.chart(table(citibike$usertype), ylab="Frequency", main='Pareto chart for User Types')
+pareto.chart(table(citibike$gender), ylab="Frequency", main='Pareto chart for User Gender', xlab= 'Gender (0 = unknown, 1 = male, 2 = female)')
+pareto.chart(table(citibike$start.station.id), ylab="Frequency", main='Pareto chart for Pick up station')
+pareto.chart(table(citibike$end.station.id), ylab="Frequency", main='Pareto chart for Drop off station')
 
 
 
